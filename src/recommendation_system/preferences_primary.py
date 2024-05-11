56bd23923cc8cfb097c46e67e681801ab19bc8b7
cru23110 <fue23775@uvg.edu.gt>
2024-05-11 14:09:28 -0600

    primary features are generated successfully

--- src/recommendation_system/preferences_primary.py
+++ src/recommendation_system/preferences_primary.py
@@ -1,3 +1,4 @@
+import random
 import requests
 from bs4 import BeautifulSoup
 from db.neo4j_config import neo4j_connection
@@ -20,7 +21,6 @@ class PrimaryPreferences:
                 pp_node = record["pp"]
                 nombre = pp_node["nombre"]
                 valor = pp_node["valor"]
-                print(user_id,nombre,valor)
                 # Mapear nombres de preferencias a nombres más descriptivos
                 preference_name = self.map_preference_name(nombre)
                 
@@ -49,20 +49,87 @@ class PrimaryPreferences:
         }
         return mapping.get(nombre, nombre)
 
-def obtener_nombre(genero):
-    if genero.lower() == 'hombres':
-        url = "https://generadordenombres.online/hombre/"
-    elif genero.lower() == 'mujeres':
-        url = "https://generadordenombres.online/mujer/"
-    else:
-        print("Género no válido.")
-        return None
+    def obtener_nombre(self, genero):
+        if genero.lower() == 'hombres':
+            url = "https://generadordenombres.online/hombre/"
+        elif genero.lower() == 'mujeres':
+            url = "https://generadordenombres.online/mujer/"
+        else:
+            print("Género no válido.")
+            return None
 
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        nombre = soup.find('span', class_='svelte-6735sq').text
-        return nombre
-    else:
-        print("Error al obtener la página:", response.status_code)
-        return None
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            nombre = soup.find('span', class_='svelte-6735sq').text
+            return nombre
+        else:
+            print("Error al obtener la página:", response.status_code)
+            return None
+
+    def generate_profile(self, user_id):
+        new_profile = {}
+
+        primary_preferences = PrimaryPreferences().get_primary_preferences(user_id)
+        # print(primary_preferences)
+        
+        # Generar nombre de usuario basado en el género
+        genero = primary_preferences.get('Preferencia de género')
+        if genero:
+            new_profile['Nombre'] = self.obtener_nombre(genero)
+        else:
+            new_profile['Nombre'] = None
+
+        # Asignar género del nuevo perfil
+        new_profile['Género'] = 'Mujer' if genero == 'mujeres' else 'Hombre'
+
+        # Generar valores aleatorios para edad, altura y distancia dentro de los rangos dados
+        new_profile['Edad'] = random.randint(int(primary_preferences.get('Edad mínima')), int(primary_preferences.get('Edad máxima')))
+        new_profile['Altura'] = random.randint(int(primary_preferences.get('Altura mínima')), int(primary_preferences.get('Altura máxima')))
+        new_profile['Distancia'] = random.randint(int(primary_preferences.get('Distancia mínima')), int(primary_preferences.get('Distancia máxima')))
+
+        # Obtener intereses y elegir dos al azar
+        intereses_usuario = primary_preferences.get('Intereses', [])
+        if intereses_usuario:
+            intereses_elegidos = random.sample(intereses_usuario, min(len(intereses_usuario), 2))
+            new_profile['Intereses'] = intereses_elegidos
+        else:
+            new_profile['Intereses'] = []
+
+        # Asignar religión y otro tipo de religión según lo especificado
+        religion = primary_preferences.get('Religión')
+        other_religion = primary_preferences.get('Otra religión')
+        if religion == 'otro':
+            new_profile['Religión'] = other_religion
+        else:
+            new_profile['Religión'] = religion
+
+        # Asignar tipo de relación según lo especificado
+        relationship_type = primary_preferences.get('Tipo de relación')
+        other_relationship = primary_preferences.get('Otro tipo de relación')
+        if relationship_type == 'otras':
+            new_profile['Tipo de relación'] = other_relationship
+        else:
+            new_profile['Tipo de relación'] = relationship_type
+
+        # Asignar estado civil
+        estado_civil = primary_preferences.get('Estado civil')
+        if estado_civil:
+            if genero == 'mujeres':
+                if 'a' in estado_civil:
+                    estado_civil = estado_civil[:-2]
+            else:
+                if 'a' not in estado_civil:
+                    estado_civil += 'o'
+            new_profile['Estado civil'] = estado_civil
+        else:
+            new_profile['Estado civil'] = None
+
+        # Asignar preferencia de fumador y bebedor
+        new_profile['Preferencia de fumador'] = 'No fumador' if primary_preferences.get('Preferencia de fumador') == 'no' else 'Fumador'
+        new_profile['Preferencia de bebedor'] = 'No bebedor' if primary_preferences.get('Preferencia de bebedor') == 'no' else 'Bebedor'
+
+        # Asignar nivel educativo
+        new_profile['Nivel educativo'] = primary_preferences.get('Nivel educativo')
+
+        return new_profile
--- src/recommendation_system/profile_generation_manager.py
+++ src/recommendation_system/profile_generation_manager.py
@@ -1,28 +1,8 @@
-from src.recommendation_system.preferences_primary import PrimaryPreferences, obtener_nombre
+from src.recommendation_system.preferences_primary import PrimaryPreferences
 
 def generate_new_profile(user_id):
-    # Obtener las preferencias primarias del usuario
-    primary_preferences = PrimaryPreferences().get_primary_preferences(user_id)
-    print("Preferencias primarias del usuario:", primary_preferences)
-
-    # Obtener el género del usuario
-    genero = primary_preferences.get('Preferencia de género')
-
-    # Generar el nombre de usuario basado en el género
-    print(genero)
-    if genero:
-        nombre_usuario = obtener_nombre(genero)
-        if nombre_usuario:
-            print("Nombre de usuario generado:", nombre_usuario)
-            # Lógica para generar el nuevo perfil...
-            # Devolver True si el proceso fue exitoso
-            return True
-        else:
-            print("No se pudo obtener el nombre de usuario.")
-            return False
-    else:
-        print("No se pudo obtener la preferencia de género del usuario.")
-        return False
-    
+    primary_preferences = PrimaryPreferences()
+    nuevo_perfil = primary_preferences.generate_profile(user_id)
+    print(nuevo_perfil)
     # Devolver True si el proceso fue exitoso
     return True
